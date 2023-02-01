9bc1ef64acb1b2e5c7855d67264697a39b11d78e
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-02-01 13:17:45 +0300

    Rubocop applied

--- basedecorator.rb
+++ basedecorator.rb
@@ -1,12 +1,12 @@
 require_relative 'decorator'
 
 class BaseDecorator < Nameable
-    def initialize(nameable)
-        super()
-        @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
--- capitalizedecorator.rb
+++ capitalizedecorator.rb
@@ -1,7 +1,7 @@
 require_relative 'basedecorator'
 
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-      @nameable.correct_name.capitalize
-    end
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
