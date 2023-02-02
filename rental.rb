e27dda09c142ca29c36d22f2aefcd3edd52fd3a7
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-02-02 18:42:15 +0300

    Corrects the linter errors

--- rental.rb
+++ rental.rb
@@ -1,12 +1,11 @@
 class Rental
-  
-    def initialize(date, book, person)
-      @date = date
-      @book = book
-      book.rental << self
-      @person = person
-      person.rental << self
-    end
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    book.rental << self
+    @person = person
+    person.rental << self
+  end
 
-    attr_accessor :date, :book, :person
+  attr_accessor :date, :book, :person
 end
