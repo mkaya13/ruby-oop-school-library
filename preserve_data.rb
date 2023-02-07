f553755bcbe5dc6105c613c36838fdcebe261e97
Wassim <wassimchakib@hotmail.com>
2023-02-07 15:19:32 +0100

    Fix linter errors

--- preserve_data.rb
+++ preserve_data.rb
@@ -2,7 +2,6 @@ require 'fileutils'
 require 'json'
 
 FileUtils.mkdir_p('saved_data')
-base = "#{Dir.pwd}/saved_data"
 # File.open("#{base}/books.json", 'w')
 # File.open("#{base}/people.json", 'w')
 # File.open("#{base}/rentals.json", 'w')
