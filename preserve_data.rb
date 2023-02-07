a6042ebdb6ae97c01548139fe3fede9b998c0f2f
Wassim <wassimchakib@hotmail.com>
2023-02-07 15:19:03 +0100

    Add utility functions to save the date into json files

--- preserve_data.rb
+++ preserve_data.rb
@@ -1,5 +1,42 @@
+require 'fileutils'
+require 'json'
+
 FileUtils.mkdir_p('saved_data')
 base = "#{Dir.pwd}/saved_data"
-File.open("#{base}/books.json", 'w+')
-File.open("#{base}/people.json", 'w+')
-File.open("#{base}/rentals.json", 'w+')
+# File.open("#{base}/books.json", 'w')
+# File.open("#{base}/people.json", 'w')
+# File.open("#{base}/rentals.json", 'w')
+
+def write_data(filename, array)
+  case filename
+  when 'people'
+    write_people(array)
+  when 'rentals'
+    write_rentals(array)
+  when 'books'
+    write_books(array)
+  else
+    puts 'Error!'
+  end
+end
+
+def write_books(array)
+  base = "#{Dir.pwd}/saved_data"
+  empty_array = []
+  array.each { |e| empty_array.push({ title: e.title, author: e.author }) }
+  File.write("#{base}/books.json", empty_array.to_json, mode: 'a')
+end
+
+def write_rentals(array)
+  base = "#{Dir.pwd}/saved_data"
+  empty_array = []
+  array.each { |e| empty_array.push({ date: e.date, book: e.book.title, person: e.person.id }) }
+  File.write("#{base}/rentals.json", empty_array.to_json, mode: 'a')
+end
+
+def write_people(array)
+  base = "#{Dir.pwd}/saved_data"
+  empty_array = []
+  array.each { |e| empty_array.push({ person: e.class, name: e.name, id: e.id, age: e.age }) }
+  File.write("#{base}/people.json", empty_array.to_json, mode: 'a')
+end
